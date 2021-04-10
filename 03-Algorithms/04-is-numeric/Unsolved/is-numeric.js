d2cba62fe8c0d834aff6ab44029d90d42e240816
Arpita Saha <arpita.sahaa@gmail.com>
2021-04-10 12:45:03 -0400

    algo 04

--- 03-Algorithms/04-is-numeric/Unsolved/is-numeric.js
+++ 03-Algorithms/04-is-numeric/Unsolved/is-numeric.js
@@ -1,3 +1,16 @@
 // Write code to create a function that accepts a string and determines if the string is a valid number. You may not use any regex, built-in methods type conversion to accomplish this.
 
-var isNumeric = function(str) {};
+const isNumeric = (str)=> {
+    let strNo = "1234567890";
+    let isValidNo;
+    for (i=0; i<str.length; i++){
+        if(strNo.includes(str[i])){
+            isValidNo = true;
+        }else{
+            isValidNo = false;
+        }
+    }
+    console.log(isValidNo);
+};
+
+isNumeric("12bc")
