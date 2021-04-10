c18628fe0eb491bf13d627ac7e4c6f862b077697
Arpita Saha <arpita.sahaa@gmail.com>
2021-04-10 11:59:23 -0400

    unsolved algo 01

--- 03-Algorithms/01-reverse-no-built-in/Unsolved/reverse-no-built-in.js
+++ 03-Algorithms/01-reverse-no-built-in/Unsolved/reverse-no-built-in.js
@@ -1,6 +1,12 @@
 // Write code to return a new string with all of the characters in `str` reversed.
 // You may NOT use the built-in reverse method
 
-var reverse = function(str) {
-
+const reverse = (str) => {
+    let reverseStr = "";
+    for(i=str.length - 1; i>=0 ; i--){
+        reverseStr = reverseStr + str[i]
+    }
+    console.log(reverseStr);
 };
+
+reverse("Bandana");
